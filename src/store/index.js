36068a800ebbe95b9404f717ad2ef4714894c28b
Victor-Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-05-05 21:14:15 -0300

    feat[data]: get posts from api

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    posts: []
   },
   mutations: {
+    savePosts (state, payload) {
+      Object.assign(state.posts, payload)
+    }
   },
   actions: {
+    getPosts (context) {
+      return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(r => r.json())
+        .then(body => {
+          context.commit('savePosts', body)
+          return body
+        })
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      posts: []
+    }
+  },
+  methods: {
+    ...mapActions(['getPosts'])
+  },
+  mounted () {
+    this.getPosts()
+      .then(posts => (this.posts = [...posts]))
   }
 }
 </script>
