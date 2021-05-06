341d1fe000d9145934221f77e3f9a85d168ae5d5
Victor-Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-05-05 21:32:38 -0300

    feat[posts]: get posts from api and show v0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="home">
-
+    <PostItem v-for="post in posts" :key="post.id" :post="post"/>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import PostItem from '@/components/PostItem.vue'
 
 export default {
   name: 'Home',
+  components: { PostItem },
+  mounted () {
+    this.getPosts()
+      .then(posts => (this.posts = [...posts]))
+  },
   data () {
     return {
       posts: []
@@ -16,10 +22,6 @@ export default {
   },
   methods: {
     ...mapActions(['getPosts'])
-  },
-  mounted () {
-    this.getPosts()
-      .then(posts => (this.posts = [...posts]))
   }
 }
 </script>
