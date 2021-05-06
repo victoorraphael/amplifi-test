1aafbe2ca2af2a286666d33053877e0810fd635f
Victor-Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-05-05 21:41:11 -0300

    feat[component]: create PostItem v0

---
+++ src/components/PostItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="post">
+    <p class="post__title">{{ post.title | capitalize }}</p>
+    <small>{{ post.body | preview }}</small>
+  </div>
+</template>
+
+<script>
+import string from '@/mixins/string'
+
+export default {
+  name: 'PostItem',
+  mixins: [string],
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped="true">
+.post {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+
+  &__title {
+    font-weight: 600;
+  }
+}
+</style>
