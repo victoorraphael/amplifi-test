3b28448ce7cde3e8c9abcd06cc32a2301795ad20
Victor-Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-05-05 21:40:51 -0300

    feat[filtes]: add filter preview

--- src/mixins/string.js
+++ src/mixins/string.js
@@ -2,6 +2,9 @@ export default {
   filters: {
     capitalize (value) {
       return value[0].toUpperCase() + value.slice(1)
+    },
+    preview (value) {
+      return value.slice(0, 30) + '...'
     }
   }
 }
