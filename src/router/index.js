ccd8972e3c73548dce12a4604116d47be105ca29
Victor-Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-05-05 22:14:13 -0300

    feat[route]: add route to comments

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import PostComments from '../views/PostComments.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'PostsList',
     component: Home
+  },
+  {
+    path: '/posts/comments/:postId',
+    name: 'PostComments',
+    component: PostComments
   }
 ]
 
